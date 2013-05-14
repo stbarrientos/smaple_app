ca92ff62f48a43ad7ed5520f2730daca49c17a32
SamBarrientos <stbarrientos@gmail.com>
2013-05-14 10:23:21 -0400

    With Excercise 1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,17 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact Page" do
+  	it "should have the h1 'Contact Page'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('h1', :text => 'Contact Page')
+  	end
+
+  	it "should have the title 'Contact Page'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact Page")
 end
+end
+end
+
